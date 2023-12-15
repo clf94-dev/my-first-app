308fa091638bf1427e8a2a70a5e392ba29d01895
DESKTOP-732UDSA\carmen <clf94@hotmail.es>
2023-12-15 17:30:47 +0100

    - add content component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import Header from './components/Header'
+import Content from './components/Content'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
+      <Content />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
